6351972c1bcb9f7783252cbb0087874adb408d0d
Amy Ly <amyly6393@gmail.com>
2017-12-20 16:36:34 -0500

    create tabs

--- Navigation.js
+++ Navigation.js
@@ -1,13 +1,36 @@
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+import React from 'react';
 import ArticleList from './app/components/article-list';
 import ArticleDetails from './app/components/article-details';
 
-const Navigation = StackNavigator({
+const ArticleNavigator = StackNavigator({
   Home: {
     screen: ArticleList
   },
   Details: {
     screen: ArticleDetails
+  }
+})
+
+const Navigation = TabNavigator({
+  TabItem1: {
+    screen: ArticleNavigator,
+    navigationOptions: {
+      tabBarLabel: "Explore",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name='search' color={tintColor} />
+      )
+    }
+  },
+  TabItem2: {
+    screen: ArticleNavigator,
+    navigationOptions: {
+      tabBarLabel: "Saved",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name='rowing' color={tintColor} />
+      )
+    }
   }
 })
 
